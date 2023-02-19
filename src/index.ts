1a08bf3bac49a76b50624d5a96a9b9585cce2df3
Nico Beierle <nico.beierle@bztes.de>
2023-02-19 22:14:41 +0100

    chore: add missing type and class exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export { undoStackStore } from './undo-stack';
 export { TransactionCtrl } from './transaction';
+export { InitAction } from './action/action-init';
+export { GroupAction } from './action/action-group';
+export { SetAction } from './action/action-set';
+export { MutateAction } from './action/action-mutate';
+export type { Immutable } from 'immer';
+export type { Objectish } from 'immer/dist/internal';
+export type { SavedUndoStack } from './undo-stack';
