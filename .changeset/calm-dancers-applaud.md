a6367542d6a25f45dce712b816a8dbf78bb67c0a
Nico Beierle <nico.beierle@bztes.de>
2023-09-01 21:10:17 +0200

    build: typescript type definitions were in the wrong folder

---
+++ .changeset/calm-dancers-applaud.md
@@ -0,0 +1,5 @@
+---
+'@gira-de/svelte-undo': patch
+---
+
+fix missing typescript types
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [dts({ outputDir: 'dist/types' })],
+  plugins: [dts({ outDir: 'dist/types' })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
